301b97219b40d9a63ee1b905549ae1a875272acc
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-12-16 13:01:29 -0500

    first unit test for users created. Currently at expected failure.

--- users/users_tests/tests.py
+++ users/users_tests/tests.py
@@ -1,6 +1,23 @@
-from django.test import TestCase
+'''
+1. An HTTP request comes in for a particular URL
+2. Django uses some rules to decide which view function should deal with the request (this is referred to as respolving the URL)
+3. The view function processes the request and returns and HTTP response
+'''
 
-class SmokeTest(TestCase):
-    def test_math(self):
-        self.assertEqual(1 + 1, 2)
+'''
+We test two things:
+1. Can we resolve the requested URL to a particular view function we've made?
+2. Can we make this view function return HTML which will get the functional test to pass?
+'''
 
+from django.urls import resolve
+from django.test import RequestFactory, TestCase
+from users.views import RegisterView
+
+
+class RegisterPageTest(TestCase):
+
+    def test_url_resolves_to_register_page_view(self):
+        resolver = resolve('/register')
+        self.assertEqual(resolver.func.view_class, RegisterView)
+    
