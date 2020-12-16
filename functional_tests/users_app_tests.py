830f0d1587da151c8ad36571013b91d6c6b65a67
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-12-15 21:49:10 -0500

    created user_can_create_account user story in comments

---
+++ functional_tests/users_app_tests.py
@@ -0,0 +1,28 @@
+from .base import FunctionalTest
+
+class UsersTest(FunctionalTest):
+
+    def test_user_can_create_account(self):
+        # John goes to the registration page
+
+        # he is greeted by the registration title and header
+
+        # he sees the form
+
+        # enters his first name
+
+        # enters his email
+
+        # submits the form
+
+        # he is taken to a new page
+
+        # the page tells him an email has been sent with a confirmation link
+
+        # John goes to his email and clicks the link
+
+        # John is taken to his account page
+
+        # There is a message that welcomes him personally
+
+        
