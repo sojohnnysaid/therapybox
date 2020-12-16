aef48ff50fe545a0d3c615e005b9bc0d2d746280
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-12-15 21:11:19 -0500

    moved ft to their own folder

--- functional_tests/django_install_tests.py
+++ functional_tests/django_install_tests.py
@@ -3,7 +3,10 @@ from django.test import LiveServerTestCase
 from django.urls import reverse
 
 class FirstTest(LiveServerTestCase):
+
     def test_first(self):
+    # our django install is working properly
         browser = webdriver.Firefox()
         browser.get(self.live_server_url + reverse('debug'))
         assert 'Django' in browser.title
+
