cd7f2f490e485ed6d7e3ea3427f6a14a198812c5
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-12-21 16:10:19 -0500

    created CustomUserTest and first unit test for custom user spike

--- CustomUser spike/notepad.md
+++ CustomUser spike/notepad.md
@@ -12,3 +12,16 @@ and override the create_user() and create_superuser()
 2. create a CustomUser(AbstractBaseUser) and include
 the appropriate attributes and methods
 
+3. create a form for the front end
+that inherits from UserCreationForm
+
+4. on the admin.py side we create:
+ - UserCreationForm
+ - UserChangeForm
+ - UserAdmin (to configure the way the admin panel looks)
+
+5. Register the CustomUser and UserAdmin
+in admin.py
+
+6. add AUTH_USER_MODEL to settings.py
+('users.CustomUser')
--- users/users_tests/custom_user_tests.py
+++ users/users_tests/custom_user_tests.py
@@ -1,10 +1,29 @@
 '''
-steps required to create a custom user model:
-1. add AUTH_USER_MODEL = 'users.CustomUser' to therapybox.settings
-2. Register the model in the users app admin.py
-3. create the model inheriting from django.contrib.auth.models.AbstractUser
+1. create CustomUserManager(BaseUserManager)
+and override the create_user() and create_superuser()
 
-3. create a custom user manager
-4. define the create_user and create_superuser methods
+2. create a CustomUser(AbstractBaseUser) and include
+the appropriate attributes and methods
 
+3. create a form for the front end
+that inherits from UserCreationForm
+
+4. on the admin.py side we create:
+ - UserCreationForm
+ - UserChangeForm
+ - UserAdmin (to configure the way the admin panel looks)
+
+5. Register the CustomUser and UserAdmin
+in admin.py
+
+6. add AUTH_USER_MODEL to settings.py
+('users.CustomUser')
 '''
+
+from django.test import TestCase
+from users.managers import CustomUserManager
+
+class CustomUserTest(TestCase):
+    
+    def test_CustomUserManager_has_create_user_method(self):
+        assert 'create_user' in dir(CustomUserManager)
