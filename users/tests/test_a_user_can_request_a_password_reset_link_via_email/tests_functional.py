754c3ad96e33b281dbb27d0b5e2103e5a1f846ca
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-12-31 21:59:41 -0500

    feature: a user can request a password reset link via email completed

--- users/tests/test_a_user_can_request_a_password_reset_link_via_email/tests_functional.py
+++ users/tests/test_a_user_can_request_a_password_reset_link_via_email/tests_functional.py
@@ -10,6 +10,7 @@ from selenium.webdriver.firefox.options import Options
 
 from django.test import LiveServerTestCase
 from django.urls import reverse
+from django.core import mail
 
 
 class BaseFunctionalTest(LiveServerTestCase):
@@ -23,10 +24,82 @@ class BaseFunctionalTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def user_registers_and_activates_account(self, name):
+
+        self.browser.get(self.live_server_url + reverse('users:register'))
+        
+        # enters email
+        input = self.browser.find_elements(By.ID, 'id_email')[0]
+        input.send_keys(f'{name}@gmail.com')
+        
+        # enters first name
+        input = self.browser.find_elements(By.ID, 'id_first_name')[0]
+        input.send_keys(f'{name}')
+
+        # enters password
+        input = self.browser.find_elements(By.ID, 'id_password1')[0]
+        input.send_keys('p@assW00rd')
+
+        # enters password to confirm
+        input = self.browser.find_elements(By.ID, 'id_password2')[0]
+        input.send_keys('p@assW00rd')
+
+        # submits the form
+        self.browser.find_elements(By.ID, 'users_register_form_submit_button')[0].click()
+
+        # goes to email...
+        email = mail.outbox[0]
+        
+        url_search = re.search(r'http://.+/users/account-activation/\?uid=.+&token=.+$', email.body)
+        
+        if not url_search:
+            self.fail(f'Could not find url in email body:\n{email.body}')
+
+        activate_account_url = url_search.group(0)
+
+        # clicks the link
+        self.browser.get(activate_account_url)
 
 
 
-class MyFunctionalTest(BaseFunctionalTest):
+
+class UserCanRequestPasswordResetLinkViaEmail(BaseFunctionalTest):
+
     
-    def test_a_story_from_the_user_perspective(self):
-        pass
+    def test_user_can_request_password_reset_link_via_email(self):
+
+        self.user_registers_and_activates_account('John')
+
+        # John goes to the login page
+        self.browser.get(self.live_server_url + reverse('users:login'))
+
+        # he is greeted by the login header
+        assert 'Login' in self.browser.find_elements(By.TAG_NAME, 'h1')[0].text
+
+        # John can't remember his password and notices a forgot password link
+        # He clicks the link
+        self.browser.find_elements(By.LINK_TEXT, 'Forgot Password? Click here!')[0].click()
+
+        # he is taken to the password reset form page
+        assert reverse('users:password_request_reset_link') in self.browser.current_url
+
+        # he is greeted by the password reset header
+        assert 'Password Reset' in self.browser.find_elements(By.TAG_NAME, 'h1')[0].text
+
+        # he sees a form with a field to enter his email
+        input = self.browser.find_elements(By.ID, 'id_email')[0]
+        assert input.get_attribute('placeholder') == 'email'
+
+        # he enters his email
+        input.send_keys('john@gmail.com')
+
+        # submits the form
+        self.browser.find_elements(By.ID, 'users_password_reset_request_form_submit_button')[0].click()
+
+        # he is taken back to the login page
+        assert reverse('users:login') in self.browser.current_url
+
+        # the page tells him an email has been sent with a confirmation link
+        form_submitted_message = self.browser.find_elements(By.CLASS_NAME, 'message')[0].text
+        assert 'Success! A password reset link was sent to your email.' in form_submitted_message
+
