3dca5b42ad670071df038a0e663c529ec629f70c
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-04 06:09:52 -0500

    added testsetup command

---
+++ core/management/commands/testsetup.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+ 
+import subprocess
+from sys import stdout, stdin, stderr
+import time, os, signal
+ 
+ 
+class Command(BaseCommand):
+    help = 'Run all commands'
+    commands = [
+        'python manage.py migrate',
+        'python manage.py addsuperuser',
+    ]
+ 
+    def handle(self, *args, **options):
+        for command in self.commands:
+            words = command.split()
+            subprocess.run([words[0], words[1], words[2]])
