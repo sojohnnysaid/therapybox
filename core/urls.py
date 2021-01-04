429def7e288d004d3287ffd6a129f611779186dc
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-04 07:11:51 -0500

    updated core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from django.views.debug import default_urlconf
 
 urlpatterns = [
-    path('', include('therapybox.urls')),
-    path('debug/', default_urlconf, name='debug'),
-    path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('', include('therapybox.urls')),
+    path('admin/', include('administration.urls')),
+    path('debug/', default_urlconf, name='debug'),
+    path('control-panel/', admin.site.urls),
 ]
