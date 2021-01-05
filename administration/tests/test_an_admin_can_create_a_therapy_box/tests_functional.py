c4d187c0798f46bfba5e103f8724c4312fafe70c
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-04 19:19:43 -0500

    feature complete an admin can add photos to a therapy box using cloudinary api

--- administration/tests/test_an_admin_can_create_a_therapy_box/tests_functional.py
+++ administration/tests/test_an_admin_can_create_a_therapy_box/tests_functional.py
@@ -18,7 +18,7 @@ class BaseFunctionalTest(LiveServerTestCase):
     def setUp(self):
         options = Options()
         options.headless = True
-        self.browser = webdriver.Firefox(options=options)
+        self.browser = webdriver.Firefox()#options=options
         self.browser.implicitly_wait(15)
         
         self.email = 'test@gmail.com'
@@ -85,11 +85,54 @@ class AdminLogsInAndCreatesTherapyBoxInstance(BaseFunctionalTest):
         self.browser.find_element_by_link_text('+ Add new item').click()
 
         # he is brought to a form
+        assert '+ new item' in self.browser.find_elements(By.TAG_NAME, 'h1')[0].text
 
         # he fills out the form
+        time.sleep(1)
+
+        # name
+        input = self.browser.find_elements(By.NAME, 'name')[0]
+        input.send_keys('test_name')
+
+        # description
+        input = self.browser.find_elements(By.NAME, 'description')[0]
+        input.send_keys('test_description')
+
+        time.sleep(5)
+        # image
+        input = self.browser.find_elements(By.NAME, 'image_1')[0]
+        input.send_keys('/Users/home/Desktop/therapybox/staticfiles/test_image_1.png')
+        time.sleep(5)
+
+
+        # tags
+        input = self.browser.find_elements(By.NAME, 'tags')[0]
+        input.send_keys('test_tags')
+
+        # length
+        input = self.browser.find_elements(By.NAME, 'length')[0]
+        input.send_keys('test_length')
+
+        # height
+        input = self.browser.find_elements(By.NAME, 'height')[0]
+        input.send_keys('test_height')
+
+        # depth
+        input = self.browser.find_elements(By.NAME, 'depth')[0]
+        input.send_keys('test_depth')
+
+        # weight
+        input = self.browser.find_elements(By.NAME, 'weight')[0]
+        input.send_keys('test_weight')
+
+        time.sleep(5)
 
         # he submits the form
+        self.browser.find_elements(By.ID, 'admin_create_therapy_box_template_submit')[0].click()
+
+        time.sleep(5)
 
         # John is back on the inventory page
+        assert 'Inventory' in self.browser.find_elements(By.TAG_NAME, 'h1')[0].text
 
         # he sees his new item in the inventory list
