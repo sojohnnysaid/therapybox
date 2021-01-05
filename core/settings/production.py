7b5d70642fe319cf2b49cdc055bb119a17bb2dec
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-04 19:33:34 -0500

    updated settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,6 +2,7 @@ from core.settings.common import *
 import django_heroku
 import dj_database_url
 
+DEBUG = os.getenv('DEBUG', False)
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
