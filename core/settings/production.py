bf685979059016760295ddcb724ab1278fc3acfd
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-06 14:26:32 -0500

    updated production settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -17,6 +17,6 @@ AWS_S3_REGION_NAME = os.getenv('BUCKETEER_AWS_REGION')
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
 # Activate Django-Heroku.
 django_heroku.settings(locals(), staticfiles=False)
