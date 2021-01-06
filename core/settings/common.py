4c9979233db3bc8c009eaf73d73f28682e037713
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-06 13:56:21 -0500

    updated production settings

--- core/settings/common.py
+++ core/settings/common.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'cloudinary',
     'users.apps.UsersConfig',
     'therapybox.apps.TherapyboxConfig',
--- core/settings/production.py
+++ core/settings/production.py
@@ -8,5 +8,12 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('BUCKETEER_AWS_ACCESS_KEY_ID')
+AWS_REGION = os.getenv('BUCKETEER_AWS_REGION')
+AWS_SECRET_ACCESS_KEY = os.getenv('BUCKETEER_AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('BUCKETEER_BUCKET_NAME')
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
