d3462d0973fa89481e7a642f1e31df2d3071dc1b
John James Yzaguirre <johnyzaguirre@vassar.edu>
2021-01-07 21:21:34 -0500

    updated factories

--- factories/factories.py
+++ factories/factories.py
@@ -1,15 +1,22 @@
 import factory
+from factory import Faker
 from django.contrib.auth.hashers import make_password
-from cloudinary.models import CloudinaryField
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class TherapyBoxTemplateFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'therapybox.TherapyBoxTemplate'
 
+    image_1 = SimpleUploadedFile(name='test_image_1', content=open("static/test_uploads/test_image_1.png", 'rb').read(), content_type='image/png')
+
 
 class TherapyBoxUserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'therapybox.TherapyBoxUser'
+
+    email = Faker('email')
+    password = 'password'
+    is_active = True
 
     @classmethod
     def _create(cls, model_class, *args, **kwargs):
