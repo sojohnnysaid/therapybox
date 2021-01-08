ceec1f27747d4145daeec8206a28ff60d26627fa
johnyzaguirre <johnyzaguirre@johnyzaguirres-iMac.local>
2021-01-08 13:11:59 -0500

    new test an_admin_can_delete_multiple_therapy_box_templates_from_the_list_page

---
+++ administration/tests/test_an_admin_can_delete_multiple_therapy_box_templates_from_the_list_page/tests_unit/tests_views.py
@@ -0,0 +1,38 @@
+from unittest import skip
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
+
+
+class BaseTestCase(TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.User = get_user_model()
+        self.email = 'test@gmail.com'
+        self.password = 'password'
+        self.client = Client()
+    
+    def get_user(self):
+        return self.User.objects.create_superuser(self.email, self.password)
+
+    def login_user(self):
+        user = self.User.objects.create(email=self.email, password=self.password)
+        self.client.force_login(user)
+    
+    def login_admin(self):
+        user = self.User.objects.create(email=self.email, password=self.password)
+        user.is_admin = True
+        user.save()
+        self.client.force_login(user)
+
+
+@skip
+class ViewTest(BaseTestCase):
+
+    def test_homepage_returns_page_status_ok(self):
+        url_name = 'app:url_name'
+        response = Client().get(reverse(url_name))
+        self.assertEqual(response.status_code, 200)
